146d9f2f8408853b1ea9ad93b50414a8029ca0a7
Sergio De Luca <sdlbsso@gmail.com>
2022-10-06 16:14:27 -0300

    csrf back

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,28 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { brotliDecompressSync } from 'zlib';
-import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/auth.guard';
 import { TransferDto } from './dto/transfer.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
+  private transfer: TransferDto[] = [];
   
   @UseGuards(JwtAuthGuard)
   @Post('transferir')
   transferir(@Body() transf: TransferDto ) {
     console.log(`Se transfirió $${transf.monto} a ${transf.destinatario}`);
+    this.transfer.push(transf);
     return transf;
   }
+
+
+  @UseGuards(JwtAuthGuard)
+  @Get('transferencias')
+  getAll() {
+    return this.transfer;
+  }
+
+
 }
