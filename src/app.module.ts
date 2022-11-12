90550bcc1a13d891ff9fbc91be76cfe278f64365
Sergio De Luca <sdlbsso@gmail.com>
2022-11-12 17:08:34 -0300

    variables de entorno jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+	
+require('dotenv').config()
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:`./env/${process.env.NODE_ENV}.env`,
+      isGlobal: true
+    }),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,25 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppModule } from '../app.module';
+
+
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: "clavesecreta",
-      signOptions: { expiresIn: '48h' },
-    }),
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn:'2h'
+          }
+        }
+      }
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
